c590bae4b5102fa290dcea684074e334d6ffd24c
Alok Saxena <Alok Saxena>
2016-08-02 00:05:10 +0530

    first

---
+++ app/src/main/java/com/project/alok4/myjsonobject/MainActivity.java
@@ -0,0 +1,52 @@
+package com.project.alok4.myjsonobject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+String url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5806201,77.3150729&radius=500&types=restaurant" +
+        "&sensor=false&key=AIzaSyBYim7RY_8oUxXfBxnJCb0jjgPCDrFr790";
+RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject jsonObject) {
+                try {
+                    JSONArray jsonArray=jsonObject.getJSONArray("results");
+                    for (int i= 0;i<jsonArray.length();i++){
+                        JSONObject obj= jsonArray.getJSONObject(i);
+                        String name= obj.getString("name");
+                        Toast.makeText(MainActivity.this," "+name,Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                Toast.makeText(MainActivity.this," "+volleyError,Toast.LENGTH_LONG).show();
+
+            }
+        });
+        requestQueue.add(jsonObjectRequest);
+    }
+}

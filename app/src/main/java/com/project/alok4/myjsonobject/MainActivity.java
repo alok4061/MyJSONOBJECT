f9b63040f952bc23014b8e7aadf89315d37aeaf8
Alok Saxena <Alok Saxena>
2016-08-02 01:08:01 +0530

    second

--- app/src/main/java/com/project/alok4/myjsonobject/MainActivity.java
+++ app/src/main/java/com/project/alok4/myjsonobject/MainActivity.java
@@ -2,6 +2,7 @@ package com.project.alok4.myjsonobject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,16 +16,23 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 String url= "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=28.5806201,77.3150729&radius=500&types=restaurant" +
         "&sensor=false&key=AIzaSyBYim7RY_8oUxXfBxnJCb0jjgPCDrFr790";
 RequestQueue requestQueue;
+    ListView listView;
+    ArrayList arrayList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         requestQueue = Volley.newRequestQueue(this);
+        listView= (ListView) findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject jsonObject) {
@@ -33,7 +41,9 @@ RequestQueue requestQueue;
                     for (int i= 0;i<jsonArray.length();i++){
                         JSONObject obj= jsonArray.getJSONObject(i);
                         String name= obj.getString("name");
-                        Toast.makeText(MainActivity.this," "+name,Toast.LENGTH_LONG).show();
+                        arrayList.add(name);
+
+                      //  Toast.makeText(MainActivity.this," "+name,Toast.LENGTH_LONG).show();
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
